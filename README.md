41e68c4b294e9283cbf5a26f44b5e88011f6fb06
Andy DAR <andydar20@gmail.com>
2022-01-03 15:44:58 -0500

    Add readme file

---
+++ README.md
@@ -0,0 +1,13 @@
+Una ayudita
+=======================
+
+...
+
+Opciones:
+      --help     Muestra ayuda                                          [booleano]
+      --version  Muestra número de versión                              [booleano]
+  -b, --base     La base de la tabla de multiplicar                     [número] [requerido]
+  -l, --listar   Determina si se muestra o no la tabla de multiplicar   [booleano] [requerido] [defecto: false]
+  -h, --hasta    Determina hasta qué número multiplicar                 [número] [requerido] [defecto: 12]
+
+...
--- config/yargs.js
+++ config/yargs.js
@@ -2,20 +2,22 @@ const yargs = require('yargs')
 .option('b',{
     alias:'base',
     type:'number',
-    demandOption: true
+    demandOption: true,
+    describe:'La base de la tabla de multiplicar'
 })
 .option('l',{
     alias:'listar',
     type:'boolean',
     demandOption: true,
-    default: false
-
+    default: false,
+    describe:'Determina si se muestra o no la tabla de multiplicar'
 })
 .option('h',{
     alias:'hasta',
     type:'number',
     demandOption: true,
-    default: 12
+    default: 12,
+    describe:'Determina hasta qué número multiplicar'
 })
 .check((argv, option)=>{
             if( isNaN( argv.b ) ){
